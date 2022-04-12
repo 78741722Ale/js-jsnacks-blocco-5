37ccb091f9d2c6f437dd0ca52be51b9ba88b45ff
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-12 12:59:08 +0200

    Snack 4 completo

---
+++ snack-4/main.js
@@ -0,0 +1,32 @@
+// Scrivi una funzione che fonda due array(aventi lo stesso numero di elementi)
+// prendendo alternativamente gli elementi da uno e dall’altro
+// es. [a, b, c], [1, 2, 3] → [a, 1, b, 2, c, 3].
+
+// fonda due array(aventi lo stesso numero di elementi)
+const arrayLetterale = ["a", "b", "c"];
+const arrayNumerico = [1, 2, 3];
+console.log(arrayLetterale, arrayNumerico);
+
+/**
+ * 
+ * @param {array} first_Array --> il primo array (arrayLetterale)
+ * @param {array} second_Array --> il secondo array (arrayNumerico)
+ * @returns {array} arrayMerged --> l'array inizialmente vuoto ma poi riempito
+ */
+function merged(first_Array, second_Array) {
+    // Creo l'array vuoto in cui pusherò tutto
+    const arrayMerged = []
+    if (first_Array.length === second_Array.length) {
+        //  Ciclo nel primo array
+            first_Array.forEach((element, index) => {
+            // Pusho element nell'array vuoto
+            arrayMerged.push(element);
+            // pusho l'index nell'array vuoto 
+            arrayMerged.push(second_Array[index])
+        });
+    }
+    // Valore di ritorno
+    return arrayMerged
+}
+// Verifica alternativa
+console.log(merged(arrayLetterale, arrayNumerico));
